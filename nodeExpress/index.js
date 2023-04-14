e49c61c72466610e3465413594b3d94f62b6ebb0
Luciana-mon <luciana.monsantos@gmail.com>
2023-04-14 00:53:59 -0300

    rota post add alunos novo

--- nodeExpress/index.js
+++ nodeExpress/index.js
@@ -2,18 +2,29 @@ const express = require("express");
 const dadosAlunos = require("./alunos");
 
 const app = express();
+app.use(express.json());
 
 app.get('/alunos', (req, res) => {
     const { nome, media } = req.query;
 
-    if(nome === dadosAlunos.nome ){
-        const nome = dadosAlunos.filtrarNome
-        res.send(nome)
+    if(nome){
+        res.send(dadosAlunos.filtrarNome());
+    } else if (media){
+        res.send(dadosAlunos.filtrarMedia());
     } else {
-        res.status(404).send("Aluno não encontrado!")
+        res.json(dadosAlunos.alunos);
     }
-    
-})
+});
+
+app.post('/alunos/novo', (req, res) => {
+    const { nome, matricula, media } = req.body;
+
+    if(nome !== undefined && matricula !== undefined && media !== undefined){
+        res.json( "Aluno Adicionado!");
+    } else {
+        res.status(404).send("Aluno não adicionado!");
+    }
+});
 
 
 
